1b3e7dfef707417adb338d2b69e171fbc349683f
Dilshad Mustafa <mdilshad2016@yahoo.com>
2024-03-06 14:44:57 +0530

    update

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -5,6 +5,7 @@ import {
 } from '@supabase/auth-ui-shared'
 import supabase from '../config/supabaseClient'
 import { useNavigate } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
 
 const LoginScreen = () => {
 
@@ -20,12 +21,20 @@ const LoginScreen = () => {
 */
 
   return (
-    <Auth
+
+    <Row className="justify-content-md-center">
+      <Col xs lg="2">
+      <Auth style="width=500px; height=500"
       supabaseClient={supabase}
       
       appearance={{ theme: ThemeSupa }}
       providers={['github']}
-    />
+      />
+
+      </Col>
+    </Row>
+
+
   );
 
   };
